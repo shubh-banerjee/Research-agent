bb10bbbcb37e747516af34178fc09c81f85f8796
shubh-banerjee <subhambanerjee.temp@gmail.com>
2026-04-14 22:30:46 +0530

    feat: netlify setup + UI upgrade

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from research_agent.app import run
 
 
 if __name__ == "__main__":
+    import json
+    report_path = 'data/research_report_*.json'
+    latest_report_path = report_path.replace('data/research_report_', 'data/latest.json')
+    with open(report_path) as report_file:
+        report_data = json.load(report_file)
+    with open(latest_report_path, 'w') as latest_file:
+        json.dump(report_data, latest_file, indent=4)
     run()
